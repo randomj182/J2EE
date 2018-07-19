b573b861c99b90f0c4e1313ed65976a82e0a528d
randomj182 <983851677@qq.com>
2018-07-19 18:08:51 +0800

    springboot init

---
+++ web/springboot/src/com/random/demo/Example.java
@@ -0,0 +1,15 @@
+package com.random.demo;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class Example {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
